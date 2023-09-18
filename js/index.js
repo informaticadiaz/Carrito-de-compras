702767d54ef1de0b1e1588c36d43306874115159
Ignacio Diaz <informatica.diaz@gmail.com>
2023-09-18 16:57:45 -0300

    añadiendo commit

--- js/index.js
+++ js/index.js
@@ -2,16 +2,17 @@ const contenedorTarjetas = document.getElementById("productos-container");
 
 /** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
 function crearTarjetasProductosInicio(productos){
-  productos.forEach(producto => {
-    const nuevaBicicleta = document.createElement("div");
-    nuevaBicicleta.classList = "tarjeta-producto"
-    nuevaBicicleta.innerHTML = `
-    <img src="./img/productos/${producto.id}.jpg" alt="Bicicleta 1">
+  productos.forEach((producto, index) => {
+    const bicicletaHtml = `
+  <div class="tarjeta-producto">
+    <img src="./img/productos/${producto.id}.jpg" alt="Bicicleta 1" class="">
     <h3>${producto.nombre}</h3>
     <p class="precio">$${producto.precio}</p>
-    <button>Agregar al carrito</button>`
-    contenedorTarjetas.appendChild(nuevaBicicleta);
-    nuevaBicicleta.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
-  });
+    <button>Agregar al Pedido</button>
+  </div>`
+    contenedorTarjetas.insertAdjacentHTML("beforeend",bicicletaHtml);
+    //nuevaBicicleta.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+    document.getElementsByClassName("tarjeta-producto")[index].addEventListener("click",() => agregarAlCarrito(producto))  });
+  
 }
 crearTarjetasProductosInicio(bicicletas);
